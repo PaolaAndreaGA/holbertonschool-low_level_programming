37b263df9f7858a69700334739c29a4adad907f4
PaolaAndreaGA <paga020964gmail.com>
2021-06-18 15:24:39 -0700

    numeros base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main -  This a C program that prints the last digit.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n;
+for (n = '0'; n <= '9'; ++n)
+{
+printf("%i");
+}
+return (0);
+}
