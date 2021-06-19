8ba92326401c661e530253b3a81b32f24f3a7644
PaolaAndreaGA <paga020964gmail.com>
2021-06-18 18:04:14 -0700

    charnum

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - This a C program that prints the numbers
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c;
+for (c = '0'; c <= '9'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
