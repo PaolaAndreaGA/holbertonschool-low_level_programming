5ed919462b74edb3304d37afaa1444c620df3437
PaolaAndreaGA <paga020964gmail.com>
2021-06-18 20:43:22 -0700

    all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,13 @@
 int main(void)
 {
 int n;
-int x;
-for (n = 0; n <= 9; n++)
+for (n = 48; n <= 57; c++)
 {
-for (x = 0; x <= 9; n++)
-{
-putchar(n + '0');
-putchar(x + '0');
-if (n != 9 || x != 9)
+putchar(c);
+if (n != 57)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 putchar('\n');
