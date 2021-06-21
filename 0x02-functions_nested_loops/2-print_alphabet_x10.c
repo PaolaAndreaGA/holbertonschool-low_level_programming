339743385db5c90ab0ee9a7bf7e9ae5f67ce4b5c
PaolaAndreaGA <paga020964gmail.com>
2021-06-21 08:19:45 -0700

    alfabeto x 10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * main - this c program prints the alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+char b, k;
+for (k = 0; k = 10; k++)
+{
+for (b = "a"; b <= "z"; b++)
+{
+_putchar(b);
+}
+}
+_putchar('\n');
+return (0);
+}
