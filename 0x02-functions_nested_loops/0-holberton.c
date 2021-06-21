d77c4681cdabe40dfca3e863417b307630c61e11
PaolaAndreaGA <paga020964gmail.com>
2021-06-21 05:58:04 -0700

    write holberton

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - this a C program that prints "Holberton" with putchar funtion.
+ *
+ * return: always 0.
+ */
+int main(void)
+{
+putchar('H');
+putchar('o');
+putchar('l');
+putchar('b');
+putchar('e');
+putchar('r');
+putchar('t');
+putchar('o');
+putchar('n');
+putchar('\n');
+return (0);
+}
