cda359291d794d3cb40895bd7eae63de90e9fd4a
PaolaAndreaGA <paga020964gmail.com>
2021-06-21 07:11:36 -0700

    1 alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - this c program prints the alphabet.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void);
+{
+char b;
+for (b = "a"; b <= "z"; b++)
+{
+_putchar(b);
+}
+_putchar('\n');
+return (0);
