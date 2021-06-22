37aaf6aa3cdaaf46b9e1131be0c96b45615b3a38
PaolaAndreaGA <paga020964gmail.com>
2021-06-22 10:34:47 -0700

    tabla de multiplicar

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+/**
+ * times_table - prints the tables of 0 to 9
+ * times table
+ * Return: Void
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; i++)
+	{
+		_putchar('0');
+		_putchar(',');
+		for (b = 1; b <= 9; b++)
+		{
+			_putchar(' ');
+			if (a * b < 10)
+			{
+				_putchar(' ');
+				_putchar(a * b  + '0');
+			}
+			else
+			{
+				_putchar(a * b / 10 + '0');
+				_putchar(a * b % 10 + '0');
+			}
+			if (b < 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
