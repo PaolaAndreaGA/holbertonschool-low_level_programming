9dc22a8e22c68bd59fad1640f5060d90c2f82479
PaolaAndreaGA <paga020964gmail.com>
2021-06-22 07:17:28 -0700

    isalfa

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
  * _isalpha - this c program checks for lowercase or uppercase character.
- *
+ * @c:Number to pass to _isalpha function.
  * Return: Always 0.
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
 {
