ff5529c244282542f31737fa737d1c1566373f21
PaolaAndreaGA <paga020964gmail.com>
2021-06-22 06:26:58 -0700

    alfa10veces

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 /**
- * print_alphabet - this c program prints the alphabet.
+ * print_alphabet_x10 - this c program prints the alphabet.
  *
  * Return: Always 0.
  */
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
 char b, k;
 for (k = 0; k < 10; k++)
