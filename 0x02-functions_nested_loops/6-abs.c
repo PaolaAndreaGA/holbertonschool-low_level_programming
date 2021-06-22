c9d0af6b01eb0c62c48f8ea49c7f00e38e88b588
PaolaAndreaGA <paga020964gmail.com>
2021-06-22 07:40:15 -0700

    valor absoluto

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _abs - C program that computes the absolute value of an integer.
+ * @r: is the number that is evaluated to obtain the absolute value
+ */
+int _abs(int r)
+{
+
+	if (r >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
