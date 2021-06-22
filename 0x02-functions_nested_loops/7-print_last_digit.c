360444da312ff916dbb5c3a57ab72786ff46fcb6
PaolaAndreaGA <paga020964gmail.com>
2021-06-22 08:37:18 -0700

    ultimo digito

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @n: stores the number that must be evaluated to obtain the last digit
+ *
+ *Return: 0.
+ *
+ */
+
+int print_last_digit(int n)
+{
+	n % = 10;
+
+	if (c < 0)
+
+		c = -c
+
+			_putchar(c + '0');
+	return (c);
+}
