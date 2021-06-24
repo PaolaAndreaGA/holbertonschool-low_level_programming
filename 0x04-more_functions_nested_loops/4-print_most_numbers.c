b0ac603291debbc0bb8e0da9d5f539fec6851629
PaolaAndreaGA <paga020964gmail.com>
2021-06-24 10:39:08 -0700

    sin 2 sin 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
  *print_most_numbers - prints the numbers 0 to 9 without 2 and 4
- *return: Always 0.
  */
+
 void print_most_numbers(void)
 {
 	int c;
-	for (c = 48; c < 58 ; ++c)
+
+	for (c = 48; c < 58; ++c)
 	{
 		if (c != 50 && c != 52)
 		{
