ae9002ed9b496ad87cf8eff9c53e856c4fdded33
PaolaAndreaGA <paga020964gmail.com>
2021-06-24 21:07:39 -0700

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,23 +8,18 @@
 int main(void)
 {
 	int d;
-	for (d = 1; d <= 100; d++)
+
+	for (d = 1; d < 100; d++)
 	{
 		if (d % 3 == 0 && d % 5 == 0)
-		{
-			printf("FizzBuzz\n");
-		}
+			printf("FizzBuzz ");
 		else if (d % 3 == 0)
-		{
-			printf("Fizz\n");
-		}
+			printf("Fizz ");
 		else if (d % 5 == 0)
-		{
-		       printf("Buzz\n");
-		}
+			printf("Buzz ");
 		else
-		{
 			printf("%d ", d);
-		}
 	}
+	printf("Buzz\n");
+	return (0);
 }
