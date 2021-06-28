46b34bccd818fd5ce54d2a7f8ccf413a484fc6e5
PaolaAndreaGA <paga020964gmail.com>
2021-06-28 13:10:21 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes a pointer to an int as parameter and update the value 98
+ *@n: variable where is save number
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+
+	*n = 98;
+
+}
