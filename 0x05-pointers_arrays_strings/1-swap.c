77c1fd817a23f4fc1d6f2828917e50856c8efc60
PaolaAndreaGA <paga020964gmail.com>
2021-06-28 14:04:36 -0700

    intercambia valores

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swaps the values of two integers.
+ *@*a: pointer value a
+ *@*b: pointer value b
+ *@a: value integer
+ *@b: value integer
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int prov;
+	prov = *a;
+	*a = *b;
+	*b = prov;
+
+}
