c44ab7fb42187d6ab081c720a8b5644985ff84f3
PaolaAndreaGA <paga020964gmail.com>
2021-06-28 15:43:22 -0700

    longitud

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen - returns the length of a string.
+ *@s: variable
+ *
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+
+	int large = 0;
+
+	while (*s != '\0')
+	{
+
+		large++;
+	}
+
+	return (0);
+}
