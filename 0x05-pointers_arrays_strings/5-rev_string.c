fc6aafb4e0dc37a02611187cfdc40adcae6ec6a9
PaolaAndreaGA <paga020964gmail.com>
2021-06-28 18:57:20 -0700

    cadena reves

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - function that reverses a string.
+ *@s: variable
+ * Return: Always 0.
+ */
+
+void rev_string(char *s)
+{
+
+	int c = 0;
+	int k;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+
+	for (k = c - 1; k >= 0; k--)
+	{
+
+	}
+
+}
