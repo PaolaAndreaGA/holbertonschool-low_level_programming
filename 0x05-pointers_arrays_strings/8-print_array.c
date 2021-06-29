ab4f98697b125ce60a1d7e2d4405ec36da5ff8e6
PaolaAndreaGA <paga020964gmail.com>
2021-06-29 14:09:48 -0700

    matriz

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array -  prints n elements of an array of integers
+ *@a: variable
+ *@n: array size
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+
+	int y;
+
+	for (y = 0; y < n; y++)
+	{
+
+		printf("%d", a[y]);
+
+		if (y < (n - 1))
+		{
+
+			printf(",");
+		}
+	}
+
+	printf("\n");
+}
