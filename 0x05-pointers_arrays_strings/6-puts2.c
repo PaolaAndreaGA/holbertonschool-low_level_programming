78c5c9a7330d4087d7812b9635e284a3036ef19d
PaolaAndreaGA <paga020964gmail.com>
2021-06-29 08:19:52 -0700

    starting with the first character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ *puts2 - prints every other character of a string, starting with first charact
+ *@str: variable character
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+
+	int c;
+	int x;
+
+	while (str[c] != '\0')
+	{
+		c++;
+		if (c % 2 == 0)
+		{
+
+			x =  str[c];
+			_putchar(x);
+
+		}
+	}
+
+	_putchar('\n');
+}
