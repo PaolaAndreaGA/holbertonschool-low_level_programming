807a03ec5f20d30be1947950f8055db6c692e955
PaolaAndreaGA <paga020964gmail.com>
2021-06-29 19:51:39 -0700

    copia

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_strcpy - Copies the string pointed to by src, including the terminating null
+ *@dest: variable character
+ *@src: source to copy
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int x;
+
+	while (*(scr + x))
+	{
+
+		*dest + x = *scr + x;
+		x++;
+	}
+
+	*dest + x = '\0';
+	return (dest);
+
+}
