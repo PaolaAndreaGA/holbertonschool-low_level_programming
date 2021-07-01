0d2c9170c7046a1665efe3dc44c2ed0e0b551eaa
PaolaAndreaGA <paga020964gmail.com>
2021-07-01 13:34:43 -0700

    concatena

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_strcat - function that concatenates two strings.
+ *@dest: string
+ *@src: string
+ * Return: string
+ */
+char *_strcat(char *dest, char *src)
+{
+
+
+	while(*src)
+	{
+		src++;
+		
+	}
+	while(src++ == dest++)
+	{
+		;
+		
+	}
+	return (dest);
+}
