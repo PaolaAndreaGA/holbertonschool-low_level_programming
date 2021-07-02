1236894241cef54a103673d65546c3717041a505
PaolaAndreaGA <paga020964gmail.com>
2021-07-02 12:37:22 -0700

    cambiamayusculas

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *string_toupper - changes all lowercase letters of a string to uppercase.
+ *
+ * Return: string with char
+ */
+
+char *string_toupper(char *str)
+{
+	int x = 0;
+
+	if (str == 0)
+		return (0);
+
+
+	for (x = 0; str[x]; x++)
+	{
+
+		if (str[x] >= 97 && str[x] <= 122)
+		{
+			str[x] -= 32;
+		}
+	}
+
+	return (str);
+}
+
