40f0603d5e17037eb7b91e649bc487b289bb9bdf
PaolaAndreaGA <paga020964gmail.com>
2021-07-01 18:25:51 -0700

    concatenar

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,5 +1,5 @@
-#include "holberton.h"
-#include <stdio.h>
+ #include "holberton.h"
+  #include <stdio.h>
 
 /**
  *_strcat - function that concatenates two strings.
@@ -9,17 +9,22 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	int x = 0;
+	int y = 0;
+
+	while (dest[x] != '\0')
+
+		x++;
+
+	while (src[y] != '\0')
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
 
 
-	while(*src)
-	{
-		src++;
-		
-	}
-	while(src++ == dest++)
-	{
-		;
-		
-	}
+
 	return (dest);
+
 }
