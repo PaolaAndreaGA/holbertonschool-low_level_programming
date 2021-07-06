3fc4618e55a69879c82d8d89d3a9f70740cbd2d8
PaolaAndreaGA <paga020964gmail.com>
2021-07-06 09:52:10 -0700

    pointer to pointer

---
+++ 0x07-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * set_string -  sets the value of a pointer to a char.
+ * @s: direct parameter
+ * @to: array copy
+ * Return: c or null
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
