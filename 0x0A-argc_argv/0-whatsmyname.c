aac45c83ad579181c9eafc09d7446f4c10111fd8
PaolaAndreaGA <paga020964gmail.com>
2021-07-08 10:13:35 -0700

    my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - principal funtion
+ *@argc: array counter
+ *@argv: array character type
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s'\n'", argv[0]);
+	printf("%d'\n'", argc);
+	return (0);
+}
