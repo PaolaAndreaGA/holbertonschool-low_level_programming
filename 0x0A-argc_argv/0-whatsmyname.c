c4e9fbb677464b29a5563f8721c3b2ae7ee2f766
PaolaAndreaGA <paga020964gmail.com>
2021-07-08 12:29:25 -0700

    my name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,14 +8,8 @@
 
 int main(int argc, char *argv[])
 {
-	int x;
 
-	for (x = 0; x < argc; x++)
-	{
-		printf("%s'\n'", argv[x]);
-
-
-	}
-
+	(void)argc;
+	printf("%s'\n'", argv[x]);
 	return (0);
 }
