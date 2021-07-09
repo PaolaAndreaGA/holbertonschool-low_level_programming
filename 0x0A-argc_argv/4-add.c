771df4db864ab15e1f56d75d38bed287afcb56e3
PaolaAndreaGA <paga020964gmail.com>
2021-07-08 19:15:53 -0700

    suma

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - principal funtion
+ *@argc: array counter
+ *@argv: array character type
+ *Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, adi = 0;
+
+	if (argc < 1)
+		return (0);
+
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("Error\n");
+			return (1);
+		}
+
+		adi += atoi(argv[i]);
+	}
+
+	printf("%d\n", adi);
+	return (0);
+}
