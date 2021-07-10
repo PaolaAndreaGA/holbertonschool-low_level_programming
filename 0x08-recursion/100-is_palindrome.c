57f1a0e038ce76755435a24f454a2b23cae6aea9
PaolaAndreaGA <paga020964gmail.com>
2021-07-09 20:16:29 -0700

    palin

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *is_palindrome - returns 1 if a string is a palindrome and 0 if not.
+ *@s: variable
+ *Return: zero or one
+ */
+
+int is_palindrome(char *s)
+
+{
+	;
+}
