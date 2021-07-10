a65068cad113408e95b13245d9686c56c6d81344
PaolaAndreaGA <paga020964gmail.com>
2021-07-09 20:19:45 -0700

    2string

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *wildcmp - that compares two strings and returns
+ *@s1: variable
+ *@s2: variable
+ *Return: zero or one
+ */
+
+int wildcmp(char *s1, char *s2)
+
+{
+	;
+}
