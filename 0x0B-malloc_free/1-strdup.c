2d8658a745e29375e610d37ff634e729ec2bdeb6
PaolaAndreaGA <paga020964gmail.com>
2021-07-13 17:36:23 -0700

    strdup copia string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strdup-  returns a pointer to a newly allocated space in memory.
+ * @str: paramater array size
+
+ * Return: A pointer to the array, or NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	char *y;
+	unsigned int x = 0;
+
+	if (str)
+	{
+		while (str[x++])
+		{
+			;
+		}
+
+		y = malloc(sizeof(char) * x);
+
+		if (y)
+		{
+			while (x--)
+			{
+				y[x] = str[x];
+			}
+
+			return (y);
+		}
+	}
+	return (NULL);
+}
