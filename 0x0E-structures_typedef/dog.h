aff8283e5de1ec042bef61c695238d439b0c6684
PaolaAndreaGA <paga020964gmail.com>
2021-07-19 15:13:41 -0700

    dog library

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,4 @@
 
-
 #ifndef DOG_H
 #define DOG_H
 
@@ -17,8 +16,13 @@ struct dog
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 void _putchar(int);
 
 #endif /* DOG_H */
