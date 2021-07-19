caba404c2035f32b511a430c5c668b6d25962e99
PaolaAndreaGA <paga020964gmail.com>
2021-07-19 10:11:29 -0700

    dog library

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+
+
+#ifndef DOG_H
+#define DOG_H
+
+/**struct dog - DOG DEFINITION
+ *@name:  Dog's name
+ *@age: Dog's age
+ *@owner: Dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void _putchar(int);
+
+#endif /* DOG_H */
