b45e8adf5b29f0cf5eafe7a5423be405bb97fe98
PaolaAndreaGA <paga020964gmail.com>
2021-07-23 18:28:09 -0700

    straing

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -18,7 +18,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(str, n);
 	for (i = 0; i < n; i++)
 	{
-		s = va_arg(str, char*);
+		s = va_arg(str, char *);
 
 		if (separator == NULL)
 		{
