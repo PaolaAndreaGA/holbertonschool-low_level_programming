6829b545ce3964d0a4176ee9b40e334879060e70
PaolaAndreaGA <paga020964gmail.com>
2021-07-23 19:28:07 -0700

    string

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -15,6 +15,9 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	char *s;
 	va_list str;
 
+	if (separator == NULL)
+		return;
+
 	va_start(str, n);
 
 	for (i = 0; i < n; i++)
@@ -32,7 +35,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 		}
 
-		if (i < n - 1 && separator != NULL)
+		if (i < n - 1 && separator)
 
 			printf("%s", separator);
 
