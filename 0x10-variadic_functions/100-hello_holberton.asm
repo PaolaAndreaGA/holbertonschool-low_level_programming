7c36ad33bd3bb0710a3b04ccb88bb27b59579b4e
PaolaAndreaGA <paga020964gmail.com>
2021-07-23 20:24:30 -0700

    assambly

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,13 +1,20 @@
-	section .data
-
-	message db "Hello, Holberton", 10
-
+	global main	
 	section .text
+main:
+	
 
 	global _start
 _start:
 	mov rax, 1
 	mov rdi, 1
 	mov rsi, message
-	mov rdx, 16
-	    syscall
+	mov rdx, msglen
+	syscall
+	
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+	section .data
+	message db "Hello, Holberton", 10
+	maglen:	equ$ -msg
