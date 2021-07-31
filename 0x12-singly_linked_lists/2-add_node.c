18a11d96e673818fee2fb91504cec014f7b997cc
PaolaAndreaGA <paga020964gmail.com>
2021-07-30 19:59:45 -0700

    add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+/**
+ *add_node -  prints all the elements of a list_t list.
+ *@head: double pointer first element of list
+ *@str: pointer that duplicate
+ *Return: address of the new element or NULL if it failed.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *prov;
+
+	prov = malloc(sizeof(list_t));
+
+	if (prov == NULL)
+	{
+		return (NULL);
+	}
+
+	(*prov).next = *head;
+	(*prov).str = strdup(str);
+	(*prov).len = strlen(str);
+
+	*head = prov;
+
+	return (*head);
+}
