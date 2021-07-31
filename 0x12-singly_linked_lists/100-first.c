4568f48b8f9e99a57388f43eb3939207e8caf330
PaolaAndreaGA <paga020964gmail.com>
2021-07-30 21:17:59 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ *print  -  prints all the elements of a list_t list.
+ *Return: address of the new element or NULL if it failed.
+ */
+
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
+}
