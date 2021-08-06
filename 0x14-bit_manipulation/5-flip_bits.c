29a61e0687e35e11491edd064a6f1a7df68800d5
PaolaAndreaGA <paga020964gmail.com>
2021-08-05 20:53:30 -0700

    flip bits

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,4 @@
-#include "main.c"
+#include "main.h"
 
 /**
  *flip_bits -  returns the number of bits you would need to flip
@@ -12,6 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	int i, res;
 
 	res = 0;
+	
 	for (i = 8 * sizeof(n) - 1; i >= 0; i--)
 	{
 		if (((n ^ m) >> i) & 1)
