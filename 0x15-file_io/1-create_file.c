3c3215a6cda8c4b548e6a29c1457470dffb862fd
PaolaAndreaGA <paga020964gmail.com>
2021-08-09 18:54:28 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,25 +22,27 @@ int create_file(const char *filename, char *text_content)
 		{
 			return (-1);
 		}
-		
+
 		if (text_content == NULL)
 		{
 			size = 0;
-			
+
 			while (text_content[size] != '0')
 			{
 				size++;
 			}
 
 			fwr = write(fop, text_content, size);
-			
+
 			if (fwr < 0)
 			{
 				return (-1);
 			}
 		}
 
-	close(fop);
-	return (1);
+		close(fop);
+		return (1);
+	}
+	return (-1);
 }
 
