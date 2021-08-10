374b05f5a0db156e7a722aa603a9bdb64606964d
PaolaAndreaGA <paga020964gmail.com>
2021-08-09 21:37:06 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,36 +1,36 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * main - entry point
- * @argc: argument count
- * @argv: array of argument tokens
+ * @ac: argument count
+ * @av: array of argument tokens
  * Return: 0 on success
  */
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
 	int fd_from, fd_to, rd_stat, wr_stat;
 	mode_t perm = S_IRUSR | S_IWUSR | S_IWGRP | S_IRGRP | S_IROTH;
 	char buffer[BUFSIZE];
 
-	if (argc != 3)
+	if (ac != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	fd_from = open(argv[1], O_RDONLY);
+	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error:Can't read from file %s\n", argv[1]), exit(98);
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
+	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, perm);
 	if (fd_to == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	rd_stat = 1;
 	while (rd_stat)
 	{
 		rd_stat = read(fd_from, buffer, BUFSIZE);
 		if (rd_stat == -1)
-			dprintf(STDERR_FILENO, "Error:Can't read from file%s\n", argv[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 		if (rd_stat > 0)
 		{
 			wr_stat = write(fd_to, buffer, rd_stat);
 			if (wr_stat != rd_stat || wr_stat == -1)
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 		}
 	}
 	if (close(fd_from) == -1)
