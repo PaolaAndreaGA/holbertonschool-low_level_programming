d1fd320636ade197f475dde4291ee719a24825cf
PaolaAndreaGA <paga020964gmail.com>
2021-08-09 18:48:56 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,32 +14,31 @@ int create_file(const char *filename, char *text_content)
 {
 	int fop, size, fwr;
 
-
 	fop = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
-	if (fop == -1)
+	if (filename)
 	{
-		return (-1);
-	}
+		if (fop < 0)
+		{
+			return (-1);
+		}
+		
+		if (text_content == NULL)
+		{
+			size = 0;
+			
+			while (text_content[size] != '0')
+			{
+				size++;
+			}
 
-	if (text_content == NULL)
-	{
-		text_content = "";
-
-	}
-
-	size = 0;
-	while (text_content[size] != '0')
-	{
-		size++;
-	}
-
-	fwr = write(fop, text_content, size);
-
-	if (fwr == -1)
-	{
-		return (-1);
-	}
+			fwr = write(fop, text_content, size);
+			
+			if (fwr < 0)
+			{
+				return (-1);
+			}
+		}
 
 	close(fop);
 	return (1);
