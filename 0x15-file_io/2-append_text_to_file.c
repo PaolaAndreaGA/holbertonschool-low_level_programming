26583069b49ad7b71e743b1aea546c3d6191b1da
PaolaAndreaGA <paga020964gmail.com>
2021-08-09 20:14:27 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 
 
-	int fd, size;
+	int fd, fw, size;
 
 	if (filename == NULL)
 	{
@@ -34,9 +34,14 @@ int append_text_to_file(const char *filename, char *text_content)
 		size++;
 	}
 
-	write(fd, text_content, size);
+	fw = write(fd, text_content, size);
+
+	if (fw == -1)
+	{
+		close(fd);
+		return (-1);
+	}
 
 	close(fd);
-	return (-1);
+	return (1);
 }
-
