f517475ad977a1a1b10b056217fd36448de8762f
PaolaAndreaGA <paga020964gmail.com>
2021-08-09 19:51:33 -0700

    Speak gently, she can hear

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ *append_text_to_file - appends text at the end of a file.
+ *@filename: const char type pointer to file
+ *@text_content: conteiner
+ *Return: 1 on success, -1 on failure.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+
+	int fd, size;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
+	if (fd == -1)
+		return (-1);
+
+	size = 0;
+	while (text_content[size])
+	{
+		size++;
+	}
+
+	write(fd, text_content, size);
+
+	close(fd);
+	return (-1);
+}
+
