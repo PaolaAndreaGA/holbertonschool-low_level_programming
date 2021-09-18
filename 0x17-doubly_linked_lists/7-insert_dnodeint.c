ecfc0f0ebe76d75602ee7a196180eefd049952bb
PaolaAndreaGA <paga020964gmail.com>
2021-09-17 20:07:44 -0700

    fixed include fuction len

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,24 @@
 #include "lists.h"
 /**
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: pointer to head to a list
+ * Return: number of elements in a DLL
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t counter = 0;
+	const dlistint_t *aux = h;
+
+	while (aux != NULL)
+	{
+		counter++;
+		aux = aux->next;
+	}
+	return (counter);
+}
+
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position
  * @h: double pointer to the head of a DLL
  * @idx: the position where the new node should be entered
