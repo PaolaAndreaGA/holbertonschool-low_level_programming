590f9d347d8fbe86c941acf6e4a7c4dfe3419892
PaolaAndreaGA <paga020964gmail.com>
2021-09-17 19:27:43 -0700

    list library v2

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,11 +4,13 @@
 #include <string.h>
 #include <stdio.h>
 
-typedef struct
+typedef struct dlistint_s
 {
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 
-
-}
+} dlistint_t;
 
 
 size_t print_dlistint(const dlistint_t *h);
