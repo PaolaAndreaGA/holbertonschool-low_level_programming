513da9a3a5c97298a080f0d0a04801722eb4808b
PaolaAndreaGA <paga020964gmail.com>
2021-10-10 18:28:12 -0700

    4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,19 +14,21 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	hash_node_t *c = NULL;
 
 	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	index = key_index((unsigned char *)key, ht->size);
+	c = ht->array[idx];
+	if (c == NULL)
 	{
 		return (NULL);
 	}
-	c = ht->array[idx];
 	while (c != NULL)
 	{
-		if (!strcmp(key, c->key))
-			{
-				return (c->value);
-			}
-
+		if (!strcmp(c->key, key))
+		{
+			return (c->value);
+		}
 		c = c->next;
 	}
-
 	return (NULL);
 }
