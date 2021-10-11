a5649be5c167c7012658cb83e23c774c6e4657d1
PaolaAndreaGA <paga020964gmail.com>
2021-10-10 17:21:02 -0700

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,3 +1,4 @@
+
 #include "hash_tables.h"
 /**
  *hash_table_set - function that adds an element to the hash table.
@@ -14,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *c;
 	unsigned long int index, i;
 
-	if (ht == NULL ||*key == '\0'|| key == NULL || value == NULL)
+	if (ht == NULL || *key == '\0' || key == NULL || value == NULL)
 	{
 		return (0);
 	}
@@ -22,28 +23,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	c = strdup(value);
 	if (c == NULL)
-	{
 		return (0);
-	}
-	
 	index = key_index((const unsigned char *)key, ht->size);
 
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
-		{
 			free(ht->array[i]->value);
 			ht->array[i]->value = c;
 			return (1);
-		}
-
 	}
 	n = malloc(sizeof(hash_node_t));
 	if (n == NULL)
-	{
 		free(c);
 		return (0);
-	}
 	n->key = strdup(key);
 	if (n->key == NULL)
 	{
